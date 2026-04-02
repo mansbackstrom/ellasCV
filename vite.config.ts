703b238e43f60978ce7788e0f30519747c078a7c
mansbackstrom <mans.backstrom@hotmail.se>
2026-04-02 14:20:54 +0200

    upt

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,16 @@ import tailwindcss from '@tailwindcss/vite';
 import { ViteEjsPlugin } from 'vite-plugin-ejs';
 
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-    ViteEjsPlugin({
+    base: './',
+    plugins: [
+        tailwindcss(),
+        ViteEjsPlugin({
       // Här kan du lägga in data som du vill använda i din HTML
-      title: "Måns Bäckström - CV",
-    }),
-  ],
-  build: {
-    outDir: 'dist',
-  }
+            title: "Måns Bäckström - CV",
+        }),
+    ],
+    build: {
+        outDir: 'dist',
+        emptyOutDir: true,
+    }
 });
